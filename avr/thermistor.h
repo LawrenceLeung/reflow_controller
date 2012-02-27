cc008454646ef477db3f24c74ea2475fd8096aec
Lawrence <larryleungcal@yahoo.com>
2012-02-27 00:21:57 -0800

    Thermistor lookup table and code for epcos thermistor

---
+++ avr/thermistor.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   thermistor.h
+ * Author: lawrence
+ *
+ * Created on February 21, 2012, 11:51 PM
+ */
+
+#ifndef THERMISTOR_H
+#define	THERMISTOR_H
+
+
+#include "ovencon.h"
+    
+void thermistor_setup();    
+uint16_t thermistor_read();    
+
+
+#endif	/* THERMISTOR_H */
+
